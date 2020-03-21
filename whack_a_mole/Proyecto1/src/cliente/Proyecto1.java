64b4b6ff524924503daab79425bd40e60d0259e8
monrubi <wunderber8@hotmail.com>
2020-03-21 13:44:16 -0600

    adecuado para correr pruebas

--- whack_a_mole/Proyecto1/src/cliente/Proyecto1.java
+++ whack_a_mole/Proyecto1/src/cliente/Proyecto1.java
@@ -3,7 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyecto1;
+package cliente;
+import java.io.IOException;
+import servidor.JuegoServidor;
 
 /**
  *
@@ -14,8 +16,15 @@ public class Proyecto1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException, InterruptedException {
+        int num_jugadores = 1;
+        int puntos = 2;
+        JuegoServidor js = new JuegoServidor(num_jugadores, puntos);
+        
+        for(int i = 0; i < num_jugadores; i++){
+            String username = "Jugador"+i;
+            new HiloCliente(username).start();
+        }
     }
     
 }
